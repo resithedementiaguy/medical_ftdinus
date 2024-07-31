06df3272e102ae93c785ec60669f4d3b76a95c7a
alfaturachman <122202202929@mhs.dinus.ac.id>
2024-07-31 08:54:29 +0700

    initial commit

--- application/views/frontend/analisis_darah.php
+++ application/views/frontend/analisis_darah.php
@@ -21,14 +21,14 @@
         <div class="row match-height">
             <div class="col-md-12 col-12">
                 <div class="card">
-                    <div class="card-header">
-                        <h4 class="card-title">Analisis Darah</h4>
-                    </div>
                     <div class="card-content">
                         <div class="card-body">
                             <form class="form form-horizontal">
                                 <div class="form-body">
                                     <div class="row">
+                                        <div>
+                                            <h5 class="h5 mb-4">Informasi Pasien</h5>
+                                        </div>
                                         <div class="col-md-4">
                                             <label for="password-horizontal">NIK</label>
                                         </div>
@@ -50,92 +50,104 @@
                                             <input type="text" id="nama" class="form-control" name="nama" placeholder="Nama Lengkap" readonly>
                                         </div>
 
-                                        <div class="card-header">
-                                            <h4 class="card-title">Alat Medical</h4>
+                                        <div>
+                                            <h5 class="h5 mt-5 mb-4">Alat Medical</h5>
                                         </div>
+
                                         <div class="col-md-4">
                                             <label for="password-horizontal">Nama Alat</label>
                                         </div>
                                         <div class="col-md-8 form-group">
                                             <fieldset class="form-group">
-                                                <select class="form-select" id="basicSelect">
+                                                <select class="form-select" id="basicSelect" onchange="showFields()">
                                                     <option value="" selected hidden>Pilih Alat</option>
-                                                    <option value="">Suntik</option>
-                                                    <option value="">Ultra Sound</option>
-                                                    <option value="">Super Bright</option>
-                                                    <option value="">Magnetik</option>
+                                                    <option value="suntik">Suntik</option>
+                                                    <option value="ultraSound">Ultra Sound</option>
+                                                    <option value="superBright">Super Bright</option>
+                                                    <option value="magnetik">Magnetik</option>
                                                 </select>
                                             </fieldset>
                                         </div>
 
-                                        <div class="col-md-4">
-                                            <label for="first-name-horizontal">Glukosa</label>
-                                        </div>
-                                        <div class="col-md-8 form-group">
-                                            <input type="text" id="first-name-horizontal" class="form-control" name="fname" placeholder="Masukkan Glukosa">
-                                        </div>
-
-                                        <div class="col-md-4">
-                                            <label for="first-name-horizontal">HB</label>
-                                        </div>
-                                        <div class="col-md-8 form-group">
-                                            <input type="text" id="first-name-horizontal" class="form-control" name="fname" placeholder="Masukkan HB">
-                                        </div>
-
-                                        <div class="col-md-4">
-                                            <label for="first-name-horizontal">SPO2</label>
-                                        </div>
-                                        <div class="col-md-8 form-group">
-                                            <input type="text" id="first-name-horizontal" class="form-control" name="fname" placeholder="Masukkan SPO2">
-                                        </div>
-
-                                        <div class="col-md-4">
-                                            <label for="first-name-horizontal">Kolestrol</label>
-                                        </div>
-                                        <div class="col-md-8 form-group">
-                                            <input type="text" id="first-name-horizontal" class="form-control" name="fname" placeholder="Masukkan Kolestrol">
-                                        </div>
-
-                                        <div class="col-md-4">
-                                            <label for="first-name-horizontal">Asam Urat</label>
-                                        </div>
-                                        <div class="col-md-8 form-group">
-                                            <input type="text" id="first-name-horizontal" class="form-control" name="fname" placeholder="Masukkan Asam Urat">
-                                        </div>
-
-                                        <div class="col-md-4">
-                                            <label for="first-name-horizontal">Sinyal Ultrasound 1</label>
-                                        </div>
-                                        <div class="col-md-8 form-group">
-                                            <div class="form-group mb-3">
-                                                <textarea class="form-control" id="exampleFormControlUltraSound" rows="5" placeholder="Sinyal Ultrasound" disabled>0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1
-                                                </textarea>
+                                        <!-- Fields untuk Suntik -->
+                                        <div id="suntikFields" style="display: none;">
+                                            <div>
+                                                <h6 class="h6 mt-4 mb-4">Suntik</h6>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <label for="glukosa">Glukosa</label>
+                                            </div>
+                                            <div class="col form-group">
+                                                <input type="text" id="glukosa" class="form-control" name="glukosa" placeholder="Masukkan Glukosa">
+                                            </div>
+                                            <div class="col-md-4">
+                                                <label for="hb">HB</label>
+                                            </div>
+                                            <div class="col form-group">
+                                                <input type="text" id="hb" class="form-control" name="hb" placeholder="Masukkan HB">
+                                            </div>
+                                            <div class="col-md-4">
+                                                <label for="spo2">SPO2</label>
+                                            </div>
+                                            <div class="col form-group">
+                                                <input type="text" id="spo2" class="form-control" name="spo2" placeholder="Masukkan SPO2">
+                                            </div>
+                                            <div class="col-md-4">
+                                                <label for="kolestrol">Kolestrol</label>
+                                            </div>
+                                            <div class="col form-group">
+                                                <input type="text" id="kolestrol" class="form-control" name="kolestrol" placeholder="Masukkan Kolestrol">
+                                            </div>
+                                            <div class="col-md-4">
+                                                <label for="asamUrat">Asam Urat</label>
+                                            </div>
+                                            <div class="col form-group">
+                                                <input type="text" id="asamUrat" class="form-control" name="asamUrat" placeholder="Masukkan Asam Urat">
                                             </div>
                                         </div>
 
-                                        <div class="col-md-4">
-                                            <label for="first-name-horizontal">Sinyal Ultrasound 2</label>
-                                        </div>
-                                        <div class="col-md-8 form-group">
-                                            <div class="form-group mb-3">
-                                                <textarea class="form-control" id="exampleFormControlUltraSound" rows="5" placeholder="Sinyal Ultrasound" disabled>0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1
-                                                </textarea>
+                                        <!-- Fields untuk Ultrasound -->
+                                        <div id="ultraSoundFields" style="display: none;">
+                                            <div>
+                                                <h6 class="h6 mt-4 mb-4">Ultrasound</h6>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <label for="sinyalUltrasound">Sinyal Ultrasound</label>
+                                            </div>
+                                            <div class="col form-group">
+                                                <textarea class="form-control" id="sinyalUltrasound" rows="5" placeholder="Sinyal Ultrasound" disabled>0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1</textarea>
                                             </div>
                                         </div>
 
-                                        <div class="col-md-4">
-                                            <label for="first-name-horizontal">Sinyal Ultrasound 3</label>
+                                        <!-- Fields untuk Super Bright -->
+                                        <div id="superBrightFields" style="display: none;">
+                                            <div>
+                                                <h6 class="h6 mt-4 mb-4">Super Bright</h6>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <label for="sinyalSuperBright">Sinyal Super Bright</label>
+                                            </div>
+                                            <div class="col form-group">
+                                                <textarea class="form-control" id="sinyalSuperBright" rows="5" placeholder="Sinyal Super Bright" disabled>0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1</textarea>
+                                            </div>
                                         </div>
-                                        <div class="col-md-8 form-group">
-                                            <div class="form-group mb-3">
-                                                <textarea class="form-control" id="exampleFormControlUltraSound" rows="5" placeholder="Sinyal Ultrasound" disabled>0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1
-                                                </textarea>
+
+                                        <!-- Fields untuk Magnetik -->
+                                        <div id="magnetikFields" style="display: none;">
+                                            <div>
+                                                <h6 class="h6 mt-4 mb-4">Magnetik</h6>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <label for="sinyalMagnetik">Sinyal Magnetik</label>
+                                            </div>
+                                            <div class="col form-group">
+                                                <textarea class="form-control" id="sinyalMagnetik" rows="5" placeholder="Sinyal Magnetik" disabled>0.1, 0.2, 0.3, 0.4, 0.3, 0.2, 0.1, -0.1, -0.2, -0.3, -0.4, -0.3, -0.2, -0.1</textarea>
                                             </div>
                                         </div>
 
                                         <div class="col-sm-12 d-flex justify-content-end">
-                                            <button type="submit" class="btn btn-primary me-1 mb-1">Submit</button>
                                             <button type="reset" class="btn btn-light-secondary me-1 mb-1">Reset</button>
+                                            <button type="submit" class="btn btn-primary me-1 mb-1">Submit</button>
                                         </div>
                                     </div>
                                 </div>
@@ -149,6 +161,21 @@
 </div>
 
 <script>
+    // Menampilkan alat
+    function showFields() {
+        var selectedValue = document.getElementById("basicSelect").value;
+        var fields = ["suntikFields", "ultraSoundFields", "superBrightFields", "magnetikFields"];
+
+        fields.forEach(function(field) {
+            document.getElementById(field).style.display = "none";
+        });
+
+        if (selectedValue) {
+            document.getElementById(selectedValue + "Fields").style.display = "block";
+        }
+    }
+
+    // Tampilkan NIK
     $(document).ready(function() {
         $('#nik').change(function() {
             var nik = $(this).val();
